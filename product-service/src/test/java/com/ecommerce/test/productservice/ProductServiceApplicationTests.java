9a2a6547453452b5065703d525122c79dc9c34ea
Lucas Romero <lucasrfma@gmail.com>
2025-10-26 09:46:41 -0300

    adicionando arquivos que não estavam trackeados

---
+++ product-service/src/test/java/com/ecommerce/test/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.test.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
